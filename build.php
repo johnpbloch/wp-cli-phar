08745fbf435b3ae19a78551e874692660a9fb178
John P. Bloch <johnpbloch@users.noreply.github.com>
2023-10-23 11:16:46 -0500

    Modernize and update the project

--- build.php
+++ build.php
@@ -2,13 +2,18 @@
 
 namespace JPB;
 
-use Cz\Git\GitRepository;
+use CzProject\GitPhp\GitRepository;
 use GuzzleHttp\Client;
+use Throwable;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $git = new GitRepository(__DIR__);
-$tags = $git->getTags() ?? [];
+try {
+    $tags = $git->getTags() ?? [];
+} catch (Throwable) {
+    $tags = [];
+}
 $releases = getGithubJson('repos/wp-cli/wp-cli/releases');
 if (!is_array($releases)) {
     echo "Could not find any releases!\n";
@@ -16,17 +21,11 @@ if (!is_array($releases)) {
 }
 collect($releases)
     ->reverse()
-    ->filter(function ($release) use ($tags) {
-        return !in_array($release->tag_name, $tags);
-    })
+    ->filter(fn($release) => !in_array($release->tag_name, $tags))
     ->map(function ($release) {
         $assets = collect($release->assets);
-        $phar = $assets->first(function ($asset) {
-                return substr($asset->browser_download_url, -5) === '.phar';
-            })->browser_download_url ?? false;
-        $sig = $assets->first(function ($asset) {
-                return substr($asset->browser_download_url, -7) === '.sha512';
-            })->browser_download_url ?? false;
+        $phar = $assets->first(fn($asset) => str_ends_with($asset->browser_download_url, '.phar'))->browser_download_url ?? false;
+        $sig = $assets->first(fn($asset) => str_ends_with($asset->browser_download_url, '.sha512'))->browser_download_url ?? false;
         $tag = $release->tag_name;
         return ($phar && $sig) ? (object)compact('phar', 'sig', 'tag') : false;
     })
@@ -34,22 +33,25 @@ collect($releases)
     ->each(function ($item) use ($git) {
         $pharFile = __DIR__ . DIRECTORY_SEPARATOR . 'wp';
         $sigFile = __DIR__ . DIRECTORY_SEPARATOR . 'wp-cli.phar.sha512';
-        $client = new Client();
-        echo "Downloading $item->tag phar...\n";
-        $client->get($item->phar, ['sink' => $pharFile]);
-        echo "Downloading $item->tag signature...\n";
-        $client->get($item->sig, ['sink' => $sigFile]);
-        echo "Verifying $item->tag phar...\n";
-        $signature = trim(file_get_contents($sigFile));
-        $hash = hash_file('sha512', $pharFile);
-        if ($hash !== $signature) {
-            echo "Signature verification failed! Not committing!\n";
-            $git->checkout(['--', $pharFile]);
-        } else {
-            echo "Signature verified. Committing new phar...\n";
-            $git->addFile($pharFile);
-            $git->commit("Add $item->tag phar");
-            $git->createTag($item->tag);
+        try {
+            $client = new Client();
+            echo "Downloading $item->tag phar...\n";
+            $client->get($item->phar, ['sink' => $pharFile]);
+            echo "Downloading $item->tag signature...\n";
+            $client->get($item->sig, ['sink' => $sigFile]);
+            echo "Verifying $item->tag phar...\n";
+            $signature = trim(file_get_contents($sigFile));
+            $hash = hash_file('sha512', $pharFile);
+            if ($hash !== $signature) {
+                echo "Signature verification failed! Not committing!\n";
+                $git->execute('checkout', '--', $pharFile);
+            } else {
+                echo "Signature verified. Committing new phar...\n";
+                $git->addFile($pharFile);
+                $git->commit("Add $item->tag phar");
+                $git->createTag($item->tag);
+            }
+        } catch (Throwable) {
         }
     });
 
@@ -59,8 +61,10 @@ function getGithubJson($endpoint)
     if (!$client) {
         $client = new Client(['base_uri' => 'https://api.github.com']);
     }
-    $response = $client->get($endpoint);
-    $content = $response->getBody()->getContents();
-
-    return json_decode($content);
+    try {
+        $response = $client->get($endpoint);
+        $content = $response->getBody()->getContents();
+    } catch (Throwable) {
+    }
+    return json_decode($content ?? 'null');
 }
